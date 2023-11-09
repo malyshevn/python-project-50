f51f8b6789edb346e469021af99e31ddd9ab0647
Nikita Malyshev <babyswisha@mail.ru>
2023-11-09 15:01:11 +0300

    step 5

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -5,24 +5,25 @@ def main():
     args = parser()
     print(generate_diff(args.file_path_1, args.file_path_2))
 
+
 def generate_diff(file_path_1, file_path_2):
-        data1 = load_data(file_path_1)
-        data2 = load_data(file_path_2)
+    data1 = load_data(file_path_1)
+    data2 = load_data(file_path_2)
 
-        keys = sorted(set(data1.keys()) | set(data2.keys()))
-        diff = []
+    keys = sorted(set(data1.keys()) | set(data2.keys()))
+    diff = []
 
-        for key in keys:
-            if key in data1 and key in data2:
-                if data1[key] == data2[key]:
-                    diff.append(f"  {key}: {data1[key]}")
-                else:
-                    diff.append(f"- {key}: {data1[key]}")
-                    diff.append(f"+ {key}: {data2[key]}")
-            elif key in data1:
-                diff.append(f"- {key}: {data1[key]}")
+    for key in keys:
+        if key in data1 and key in data2:
+            if data1[key] == data2[key]:
+                diff.append(f"  {key}: {data1[key]}")
             else:
+                diff.append(f"- {key}: {data1[key]}")
                 diff.append(f"+ {key}: {data2[key]}")
+        elif key in data1:
+            diff.append(f"- {key}: {data1[key]}")
+        else:
+            diff.append(f"+ {key}: {data2[key]}")
 
         return diff
 
