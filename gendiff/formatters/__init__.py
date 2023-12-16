6ac808993b1420c290505a6b903b62edd72de06b
Никита Малышев <babyswisha@mail.ru>
2023-12-17 01:00:42 +0300

    __init__ fix

--- gendiff/formatters/__init__.py
+++ gendiff/formatters/__init__.py
@@ -1,8 +1,9 @@
 from gendiff.formatters.plain import make_plain
 from gendiff.formatters.stylish import make_stylish
+from gendiff.formatters.json import make_json
 
 __all__ = (
     "make_stylish",
     "make_plain"
-    "format_json"
+    "make_json"
 )
