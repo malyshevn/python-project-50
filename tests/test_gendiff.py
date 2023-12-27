f41efe789cef9e1d062ea78ae9f787e697741b7c
Никита Малышев <babyswisha@mail.ru>
2023-12-27 12:41:06 +0300

    test fix

--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -25,18 +25,6 @@ def test_generate_diff_yaml():
     assert generated_diff == expected_result
 
 
-def test_nested_json_stylish():
-    file1 = './tests/fixtures/tree1.json'
-    file2 = './tests/fixtures/tree2.json'
-    result = './tests/fixtures/result_tree'
-
-    generated_diff = generate_diff(file1, file2, format_name="stylish")
-    with open(result, 'r') as new_result_file:
-        expected_result = new_result_file.read()
-
-    assert generated_diff == expected_result
-
-
 def test_nested_plain():
     file1 = './tests/fixtures/tree1.json'
     file2 = './tests/fixtures/tree2.json'
