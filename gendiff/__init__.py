1041da6f5dd45aad3727a5f62a60a2d853e6c2cd
Никита Малышев <babyswisha@mail.ru>
2023-12-27 11:15:13 +0300

    ura

--- gendiff/__init__.py
+++ gendiff/__init__.py
@@ -1,5 +1,6 @@
 from gendiff.create_diff import create_diff
 from gendiff.generate_diff import generate_diff
+from gendiff.parser import parser
 
 
-__all__ = ("create_diff", "generate_diff")
+__all__ = ("create_diff", "generate_diff", "parser")
--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,19 +1,8 @@
-from gendiff.parser import parser, load_data
-from gendiff.formatters import make_stylish, make_plain, make_json
-from gendiff.create_diff import create_diff
-
+from gendiff import generate_diff, parser
 
 def main():
     first_file, second_file, format = parser()
-    data = create_diff(load_data(first_file), load_data(second_file))
-
-    match format:
-        case "stylish":
-            return make_stylish(data)
-        case "plain":
-            return make_plain(data)
-        case "json":
-            return make_json(data)
+    print(generate_diff(first_file, second_file, format))
 
 
 if __name__ == "__main__":
