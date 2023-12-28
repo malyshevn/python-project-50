e1a0f5ff904f4fe73f6b8ec479dcafd539c120d4
Никита Малышев <babyswisha@mail.ru>
2023-12-28 20:23:56 +0300

    aaa

--- gendiff/generate_diff.py
+++ gendiff/generate_diff.py
@@ -3,7 +3,7 @@ from gendiff.parser import load_data
 from gendiff.formatters.format import choose_format
 
 
-def generate_diff(file_path1, file_path2, format_name):
+def generate_diff(file_path1, file_path2, format_name="stylish"):
     file1 = load_data(file_path1)
     file2 = load_data(file_path2)
     data = create_diff(file1, file2)
