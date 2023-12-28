8156d18e7b3390f0b92d01d022a20a6ff0d560d8
Никита Малышев <babyswisha@mail.ru>
2023-12-28 19:58:33 +0300

    small fix

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -3,7 +3,7 @@ from gendiff import generate_diff, parser
 
 def main():
     first_file, second_file, format = parser()
-    print(generate_diff(first_file, second_file))
+    print(generate_diff(first_file, second_file, format))
 
 
 if __name__ == "__main__":
