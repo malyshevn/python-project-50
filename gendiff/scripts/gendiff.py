5eff21f383f6d60af995f6e3c80ae27e23e324b4
Никита Малышев <babyswisha@mail.ru>
2023-12-28 20:00:45 +0300

    idk

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -2,8 +2,8 @@ from gendiff import generate_diff, parser
 
 
 def main():
-    first_file, second_file, format = parser()
-    print(generate_diff(first_file, second_file, format))
+    first_file, second_file, format_name = parser()
+    print(generate_diff(first_file, second_file, format_name))
 
 
 if __name__ == "__main__":
