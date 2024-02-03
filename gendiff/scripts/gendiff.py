c4ca561696155007d8efb14f190f22793a778ab5
Никита Малышев <babyswisha@mail.ru>
2024-02-03 14:40:42 +0400

    small fix

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,10 +1,10 @@
-from gendiff.generate_diff import generate_difference
+from gendiff.generate_diff import generate_diff
 from gendiff.cli import parser
 
 
 def main():
     first_file, second_file, format_name = parser()
-    print(generate_difference(first_file, second_file, format_name))
+    print(generate_diff(first_file, second_file, format_name))
 
 
 if __name__ == "__main__":
